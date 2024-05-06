4e77907fdde8a378487aadc7c31bc98001dc6150
TechnoStudy <ismettechno@gmail.com>
2024-05-06 20:17:31 +0300

    _04_Tasks Task 2

--- src/test/java/_04_Tasks.java
+++ src/test/java/_04_Tasks.java
@@ -1,3 +1,10 @@
+import io.restassured.http.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class _04_Tasks {
     /**
      * Task 1
@@ -6,6 +13,74 @@ public class _04_Tasks {
      * expect content type JSON
      * expect title in response body to be "quis ut nam facilis et officia qui"
      */
+
+    @Test
+    public void Task1()
+    {
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title", equalTo("quis ut nam facilis et officia qui"))
+        ;
+
+    }
+
+    /**
+     * Task 2
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     *a) expect response completed status to be false(hamcrest)
+     *b) extract completed field and testNG assertion(testNG)
+     */
+
+
+    @Test
+    public void Task2()
+    {
+        //a)
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("completed", equalTo(false)) //hemcrest ile assertion
+        ;
+
+        //b)
+
+        boolean complatedData=
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().path("completed");
+        ;
+
+        Assert.assertFalse(complatedData);
+
+    }
+
+    /** Task 3
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * Converting Into POJO and write
+     */
+
 
 
 }
