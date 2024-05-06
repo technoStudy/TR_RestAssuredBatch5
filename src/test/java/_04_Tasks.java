843ce534fed211efa1845b52845e6b86751d1277
TechnoStudy <ismettechno@gmail.com>
2024-05-06 19:19:58 +0300

    _04_Tasks Task 1

--- src/test/java/_04_Tasks.java
+++ src/test/java/_04_Tasks.java
@@ -1,2 +1,11 @@
 public class _04_Tasks {
+    /**
+     * Task 1
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+
+
 }
