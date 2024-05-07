880b667b2537cf5b80aa01518a7df798511b36d2
TechnoStudy <ismettechno@gmail.com>
2024-05-07 21:09:49 +0300

    _06_CountryTest CreateCountryNegative Çözüm

--- src/test/java/Campus/_06_CountryTest.java
+++ src/test/java/Campus/_06_CountryTest.java
@@ -16,50 +16,56 @@ import static io.restassured.RestAssured.*;
 public class _06_CountryTest {
 
     RequestSpecification reqSpec;
-    Faker randomUreteci=new Faker();
+    Faker randomUreteci = new Faker();
+    String countryName = "";
+    String countryCode = "";
 
     @BeforeClass
-    public void LoginCampus(){
-        baseURI="https://test.mersys.io";
-         //token cookies içinde geldi
+    public void LoginCampus() {
+        baseURI = "https://test.mersys.io";
+        //token cookies içinde geldi
         // cookies i alıcam
         // request spec hazırlayacağım
         // environment varible : baseURI
         //{"username": "turkeyts", "password": "TechnoStudy123","rememberMe": "true"}
-        String userCredential="{\"username\": \"turkeyts\", \"password\": \"TechnoStudy123\",\"rememberMe\": \"true\"}";
+        String userCredential = "{\"username\": \"turkeyts\", \"password\": \"TechnoStudy123\",\"rememberMe\": \"true\"}";
 
-        Map<String,String> userCredMap=new HashMap<>();
-        userCredMap.put("username","turkeyts");
-        userCredMap.put("password","TechnoStudy123");
-        userCredMap.put("rememberMe","true");
+        Map<String, String> userCredMap = new HashMap<>();
+        userCredMap.put("username", "turkeyts");
+        userCredMap.put("password", "TechnoStudy123");
+        userCredMap.put("rememberMe", "true");
 
-        Cookies gelenCookies=
-        given()
-                //.body(userCredential)
-                .body(userCredMap)
-                .contentType(ContentType.JSON)
-                .when()
-                .post("/auth/login")
-                .then()
-                //.log().all()
-                .statusCode(200)
-                .extract().response().getDetailedCookies();
+        Cookies gelenCookies =
+                given()
+                        //.body(userCredential)
+                        .body(userCredMap)
+                        .contentType(ContentType.JSON)
+                        .when()
+                        .post("/auth/login")
+                        .then()
+                        //.log().all()
+                        .statusCode(200)
+                        .extract().response().getDetailedCookies();
         ;
 
         //System.out.println("gelenCookies = " + gelenCookies);
 
-        reqSpec= new RequestSpecBuilder()
+        reqSpec = new RequestSpecBuilder()
                 .addCookies(gelenCookies)
                 .setContentType(ContentType.JSON)
                 .build();
     }
 
     @Test
-    public void CreateCountry(){
-         //burada gelen tooken ın yine cookies içinde geri gitmesi lazım :spec
-        Map<String,String> newCountry=new HashMap<>();
-        newCountry.put("name",randomUreteci.address().country()+randomUreteci.address().countryCode());
-        newCountry.put("code",randomUreteci.address().countryCode());
+    public void CreateCountry() {
+        //burada gelen tooken ın yine cookies içinde geri gitmesi lazım :spec
+        countryName = randomUreteci.address().country() + randomUreteci.address().countryCode()
+                      + randomUreteci.address().latitude();
+        countryCode = randomUreteci.address().countryCode();
+
+        Map<String, String> newCountry = new HashMap<>();
+        newCountry.put("name", countryName);
+        newCountry.put("code", countryCode);
 
         //Not: Spec bilgileri given dan hemen sonra yazılmalı!
 
@@ -75,16 +81,31 @@ public class _06_CountryTest {
         ;
     }
 
-    @Test
-    public void CreateCountryNegative(){
+    @Test(dependsOnMethods = "CreateCountry")
+    public void CreateCountryNegative() {
         // Yukarıda gönderilen name ve codu tekrar göndererek kayıt yapılamadığını doğrulayınız.
         //burada gelen tooken ın yine cookies içinde geri gitmesi lazım:spec
+
+        Map<String, String> reNewCountry = new HashMap<>();
+        reNewCountry.put("name", countryName);
+        reNewCountry.put("code", countryCode);
+
+        given()
+                .spec(reqSpec)
+                .body(reNewCountry)
+                .when()
+                .post("/school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+        ;
 
 
     }
 
     @Test
-    public void UpdateCountry(){
+    public void UpdateCountry() {
         //burada gelen tooken ın yine cookies içinde geri gitmesi lazım:spec
 
     }
