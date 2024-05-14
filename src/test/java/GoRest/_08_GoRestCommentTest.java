140102c3b6956a6984a333b05229c19685ceebed
TechnoStudy <ismettechno@gmail.com>
2024-05-14 20:04:43 +0300

    CreateComment

---
+++ src/test/java/GoRest/_08_GoRestCommentTest.java
@@ -0,0 +1,69 @@
+package GoRest;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class _08_GoRestCommentTest {
+
+//     {
+//          "id": 95069,
+//             "post_id": 122490,
+//             "name": "Malti Kaur",
+//             "email": "kaur_malti@strosin-keebler.example",
+//             "body": "In ut vel."
+//     }
+
+
+     Faker randomUreteci=new Faker();
+     RequestSpecification reqSpec;
+     int commentID=0;
+
+
+     @BeforeClass
+     public void Setup()
+     {
+          baseURI="https://gorest.co.in/public/v2/comments";
+
+          reqSpec= new RequestSpecBuilder()
+                  .addHeader("Authorization", "Bearer f92bf3f56439b631d9ed928b3540968e747c8e75309c876420fb349cbb420ed1")
+                  .setContentType(ContentType.JSON)
+                  .build();
+     }
+
+    // Soru : CreateComment testini yapınız
+
+     @Test
+     public void CreateComment(){
+          String fullName=randomUreteci.name().fullName();
+          String email=randomUreteci.internet().emailAddress();
+          String body=randomUreteci.lorem().paragraph();
+          String postId="122490";
+
+          Map<String,String> newComment=new HashMap<>();
+          newComment.put("name",fullName);
+          newComment.put("email",email);
+          newComment.put("body",body);
+          newComment.put("post_id",postId);
+
+          given()
+
+                  .when()
+
+
+                  .then()
+
+          ;
+
+
+     }
+
+
+}
